802ee4a723ad1a290917ca70b3966463294aff10
Hassan Nawaz <hkbusnies786@gmail.com>
2024-11-19 00:53:44 +0500

    Update Project.tsx

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -24,10 +24,10 @@ const Project = () => {
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
-                    Landing Page
+                    Resume Builder
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Landing Page
+                    Resume Builder
                   </h1>
                   <p className="leading-relaxed text-stone-900">
                     The Resume Generator is an intuitive web application that streamlines the process of creating professional resumes. Users can easily input their personal details, education, work experience, and skills through a user-friendly interface. The application dynamically generates a polished resume that can be downloaded in PDF format, offering a hassle-free solution for job seekers. This project highlights my skills in form handling, data manipulation, and output generation while delivering a clean and efficient user experience.
